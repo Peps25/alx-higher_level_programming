f3d0c184ab5945450ac956ee2069f754d0e8d294
Peps25 <phelelem2@gmail.com>
2023-06-06 14:55:05 -0700

    task 13

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -4,24 +4,22 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - the singly linked list
- * @n: the int
- * @next: this will point to the next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * description: singly linked list node structure
- * for Holberton project
+ * Description: singly linked list node structure
+ *
  */
-
 typedef struct listint_s
 {
-        int n;
-        struct listint_s *next;
-}
-listint_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
 size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 
-#endif
+#endif /* LISTS_H */
