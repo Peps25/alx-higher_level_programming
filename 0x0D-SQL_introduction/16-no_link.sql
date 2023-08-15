a8458d4ec15688a7967c3455d045a792867a22fb
Peps25 <phelelem2@gmail.com>
2023-08-15 09:55:46 -0700

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,3 @@
-SELECT score, name
-FROM second_table
-HAVING name IS NOT NULL
-ORDER BY score DESC;
+-- Lists rows with name value
+-- Execute: cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score, name FROM second_table WHERE name!='' OR name IS NOT NULL ORDER BY score DESC;
