0771706a9d739d14cde6868add76dd2163aa63ed
Peps25 <phelelem2@gmail.com>
2023-08-16 09:30:58 -0700

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Print all the privileges of the USER
+-- Execute: cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+SHOW GRANTS FOR `user_0d_1`@`localhost`;
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
