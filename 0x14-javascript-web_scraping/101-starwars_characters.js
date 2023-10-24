141795a0545c257d574c4f678f5fa16bb4b417af
Peps25 <phelelem2@gmail.com>
2023-10-24 11:09:45 -0700

    task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,43 +2,45 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-//  the 'request' module to perform an HTTP GET request to the Star Wars API URL.
-request(apiUrl, function (error, response, body) {
-  // Check if there was no error during the HTTP request
-  if (!error && response.statusCode === 200) {
-    // Parse the JSON response bod
-    const movieData = JSON.parse(body);
-    // create an array of promises that fetch the data for each individual character.
-    const characterPromises = movieData.characters.map((characterUrl) => {
-      return new Promise((resolve, reject) => {
-        // Use another 'request' to fetch the data for the individual character.
-        request(characterUrl, function (charError, charResponse, charBody) {
-          // Check if there was no error during the HTTP request
-          if (!charError && charResponse.statusCode === 200) {
-            // Parse the JSON response body
-            const characterData = JSON.parse(charBody);
-            // Resolve the promise with the name of the character.
-            resolve(characterData.name);
-          } else {
-            // reject the promise with the error message if  there was an error during the HTTP request
-            reject(new Error(`Error fetching character data: ${charError}`));
-          }
-        });
-      });
+function getDataFrom (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, _res, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
     });
+  });
+}
 
-    Promise.all(characterPromises)
-      .then((characterNames) => {
-        console.log(characterNames.join('\n'));
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
-  } else {
-    console.error('Error fetching movie data:', error);
-  }
-});
+function errHandler (err) {
+  console.log(err);
+}
+
+function printMovieCharacters (movieId) {
+  const movieUri = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+  getDataFrom(movieUri)
+    .then(JSON.parse, errHandler)
+    .then(function (res) {
+      const characters = res.characters;
+      const promises = [];
+
+      for (let i = 0; i < characters.length; ++i) {
+        promises.push(getDataFrom(characters[i]));
+      }
+
+      Promise.all(promises)
+        .then((results) => {
+          for (let i = 0; i < results.length; ++i) {
+            console.log(JSON.parse(results[i]).name);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+}
+
+printMovieCharacters(process.argv[2]);
